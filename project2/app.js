3bd3514ac8619ba930a8177003cd826175cecf66
sneha ARora <arorasneha48947@gmail.com>
2025-07-26 21:18:33 +0530

    first commit

---
+++ project2/app.js
@@ -0,0 +1,8 @@
+//add new feature -- Sneh
+
+import{add,pi} from "./math.js";
+console.log(add(9,8),pi);
+
+
+import{generate} from "random-words";
+console.log(generate());
---
+++ project2/math.js
@@ -0,0 +1,8 @@
+
+
+export const sum = (a, b) => a + b;
+export const add = (a, b) => a * b;
+export const g = 9.8;
+export const pi = 3.14;
+
+
